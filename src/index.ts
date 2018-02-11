c28796711348b05897f1427d9c1310779bc2299c
Jesse Pinho <jesse@jessepinho.com>
2018-02-11 15:02:17 +0100

    feat: use request to send the tarred file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 #! /usr/bin/env node
 import * as program from 'commander'
+import * as request from 'request'
+import * as tar from 'tar'
 const packageJSON = require('../package.json') // tslint:disable-line:no-var-requires
 
 program
-  .command('deploy')
+  .command('deploy <key>')
   .option('-d, --directory <dir>', 'Specify the directory to deploy (default: `./build`)', './build')
-  .action(command => {
-    console.log(command.directory)
+  .action((key, command) => {
+    tar.create({ gzip: true }, [command.directory]).pipe(
+      request.post(`http://localhost:3000/app1/${key}`),
+    )
   })
 
 program
