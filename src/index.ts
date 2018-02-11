562c0739bde43accd20b6bb2209d9fe2ae20e4f6
Jesse Pinho <jesse@jessepinho.com>
2018-02-11 19:58:03 +0100

    refactor: make the stream part of the request, rather than piping the stream to the request

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 #! /usr/bin/env node
 import * as program from 'commander'
 import * as request from 'request'
+import { Readable } from 'stream'
 import * as tar from 'tar'
 const packageJSON = require('../package.json') // tslint:disable-line:no-var-requires
+
+const uploadStream = (stream: Readable, key: string): request.Request => {
+  const uploadRequest = request.post(`http://localhost:3000/app1/${key}`)
+  uploadRequest.form().append('package', stream, { filename: 'package.tar.gz' })
+
+  return uploadRequest
+}
 
 program
   .command('deploy <key>')
   .option('-d, --directory <dir>', 'Specify the directory to deploy (default: `./build`)', './build')
   .action((key, command) => {
-    tar.create({ gzip: true }, [command.directory]).pipe(
-      request.post(`http://localhost:3000/app1/${key}`),
-    )
+    const packageStream = tar.create({ gzip: true }, [command.directory])
+    uploadStream(packageStream, key)
+      .on('complete', console.log)
+      .on('error', console.error)
   })
 
 program
