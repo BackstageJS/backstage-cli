7264dda3b856e93c4af1332922d4398a3d89948c
Jesse Pinho <jesse@jessepinho.com>
2018-02-18 15:19:28 +0100

    feat: use rc to get config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 #! /usr/bin/env node
 import * as program from 'commander'
 import * as fs from 'fs'
+import * as rc from 'rc'
 import * as request from 'request'
 import { Readable } from 'stream'
 import * as tar from 'tar'
 const packageJSON = require('../package.json') // tslint:disable-line:no-var-requires
 
+const config = rc('backstage', {
+  tempDirectory: '/tmp',
+})
+
 const uploadStream = (file: string, key: string): request.Request => {
-  const uploadRequest = request.post(`http://localhost:3000/app1/${key}`)
+  const uploadRequest = request.post(`${config.server.baseURL}/${config.app}/${key}`)
+
   uploadRequest.form().append(
     'package',
     fs.createReadStream(file),
@@ -21,7 +27,7 @@ program
   .command('deploy <key>')
   .option('-d, --directory <dir>', 'Specify the directory to deploy (default: `./build`)', './build')
   .action((key, command) => {
-    const file = `/tmp/backstage-deploy-${new Date().getTime()}.tar.gz`
+    const file = `${config.tempDirectory}/backstage-package-${new Date().getTime()}.tar.gz`
     const packageStream = tar.create({ cwd: command.directory, file, gzip: true }, ['.'])
       .then(() => uploadStream(file, key))
   })
