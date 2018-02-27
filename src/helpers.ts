809a79cb9d32d2e700674f7874498ed372911a59
Jesse Pinho <jesse@jessepinho.com>
2018-02-27 19:48:36 +0100

    refactor: use handleError for helper errors

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,12 +2,24 @@ import * as validator from 'is-my-json-valid'
 import * as rc from 'rc'
 import { Config, schema } from './config-schema'
 
+export const handleError = (message: string) => (error: any) => {
+  // tslint:disable:no-console
+  console.error(message)
+  if (error) {
+    console.error('\n')
+    console.error(error)
+  }
+  // tslint:enable:no-console
+
+  process.exit(1)
+}
+
 export const validateConfig = (config: Config) => {
   const validate = validator(schema)
   validate(config)
   if (validate.errors) {
     const errors = validate.errors.map(error => `${error.field} ${error.message}`)
-    throw new Error(`Invalid .backstagerc:\n${errors.join('\n')}`)
+    handleError('Invalid .backstagerc:')(new Error(errors.join('\n')))
   }
 }
 
@@ -20,8 +32,8 @@ export const getConfig = (() => {
         tempDirectory: '/tmp',
       })
 
-      if (!cachedConfig.configs) {
-        throw new Error('No `.backstagerc` exists. Run `backstage init` before `backstage deploy`.')
+      if (!cachedConfig.config) {
+        handleError('No `.backstagerc` exists. Run `backstage init` before `backstage deploy`.')(new Error())
       }
     }
 
@@ -30,15 +42,3 @@ export const getConfig = (() => {
     return cachedConfig
   }
 })()
-
-export const handleError = (message: string) => (error: any) => {
-  // tslint:disable:no-console
-  console.error(message)
-  if (error) {
-    console.error('\n')
-    console.error(error)
-  }
-  // tslint:enable:no-console
-
-  process.exit(1)
-}
